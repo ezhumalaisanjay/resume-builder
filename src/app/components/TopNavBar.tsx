c06ea4970fad318a1a5ee62e111d71e5985aa3ca
ezhumalaisanjay <ezhumalaisanjay05@gmail.com>
2024-06-27 09:54:20 +0530

    add

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -13,7 +13,7 @@ export const TopNavBar = () => {
     <header
       aria-label="Site Header"
       className={cx(
-        "flex h-[var(--top-nav-bar-height)] items-center border-b-2 border-gray-100 px-3 lg:px-12",
+        "flex lg:h-[130px] items-center border-b-2 border-gray-100 px-3 lg:px-12",
         isHomePage && "bg-dot"
       )}
     >
@@ -23,7 +23,7 @@ export const TopNavBar = () => {
           <Image
             src={logoSrc}
             alt="OpenResume Logo"
-            className="h-8 w-full"
+            className="lg:h-[130px] w-full"
             priority
           />
         </Link>
